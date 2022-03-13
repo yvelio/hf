662fe93c6246ebd5a4288cb115dc6c92a55789f9
Andy Levi <andy.levi@gmx.net>
2022-03-13 13:43:08 +0100

    wio

--- history-service/src/main/java/com/yvelio/hands/repository/HandRepository.java
+++ history-service/src/main/java/com/yvelio/hands/repository/HandRepository.java
@@ -1,9 +1,11 @@
 package com.yvelio.hands.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface HandRepository extends JpaRepository<Hand, Long> {
 
+	@Query(value = "SELECT h FROM Hand h WHERE handNumber = ?1")
 	Hand findByHandNumber(Long handNumber);
 
 }
