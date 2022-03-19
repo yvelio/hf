46a52469bd981975b2813708ddea86d1c59f218d
Andy Levi <andy.levi@gmx.net>
2022-03-19 09:05:14 +0100

    Adde main class

---
+++ history-client/src/main/java/com/holdemfactory/history/client/App.java
@@ -0,0 +1,11 @@
+package com.holdemfactory.history.client;
+
+import io.quarkus.runtime.Quarkus;
+import io.quarkus.runtime.annotations.QuarkusMain;
+
+@QuarkusMain
+public class App {
+    public static void main(String ... args) {
+        Quarkus.run(args);
+    }
+}
