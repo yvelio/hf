a4dc5f2de41fbb8d4c26df1484c17cfd6d6a78de
Andy Levi <andy.levi@gmx.net>
2022-03-19 12:31:55 +0100

    wip

--- history-client/src/main/java/com/holdemfactory/history/client/App.java
+++ history-client/src/main/java/com/holdemfactory/history/client/App.java
@@ -7,5 +7,8 @@ import io.quarkus.runtime.annotations.QuarkusMain;
 public class App {
     public static void main(String ... args) {
         Quarkus.run(args);
+        System.out.println("##################################################");
+        System.out.println("			App starts...");
+        System.out.println("##################################################");
     }
 }
